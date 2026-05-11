755eb6b3b9d1276e1f5d656cbddc9b9c01b28bc8
GOPIKA-17-123 <gopikargopika12@gmail.com>
2026-05-11 12:17:49 +0530

    Initial commit

---
+++ src/test/java/com/krce/Set_Up.java
@@ -0,0 +1,26 @@
+package com.krce;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.time.Duration;
+
+public class Set_Up {
+    WebDriver driver;
+    WebDriverWait wait;
+    @BeforeTest
+    public void setUp(){
+        driver=new ChromeDriver();
+        driver.get("https://demoqa.com/");
+        driver.manage().window().maximize();
+        wait= new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+    @AfterTest
+    public void teatDown() throws InterruptedException{
+        Thread.sleep(15000);
+        driver.quit();
+    }
+}
